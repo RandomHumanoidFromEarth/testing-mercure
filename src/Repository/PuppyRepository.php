05c5bebbd32fa1db9322998f1697557ce02e1c86
mosix <ole.anders@asgoodasnew.com>
2024-10-23 13:28:00 +0200

    clean up repository

--- src/Repository/PuppyRepository.php
+++ src/Repository/PuppyRepository.php
@@ -15,29 +15,4 @@ class PuppyRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Puppy::class);
     }
-
-    //    /**
-    //     * @return Puppy[] Returns an array of Puppy objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Puppy
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
